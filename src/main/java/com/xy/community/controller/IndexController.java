707ccffb1b44d2a4b6f33a8e502c68b27418faca
lx <550120910@qq.com>
2023-04-04 17:19:17 +0800

    add check cookies

--- src/main/java/com/xy/community/controller/IndexController.java
+++ src/main/java/com/xy/community/controller/IndexController.java
@@ -1,14 +1,35 @@
 package com.xy.community.controller;
 
+import com.xy.community.dao.UserDao;
+import com.xy.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserDao userDao;
+
     @GetMapping("/")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userDao.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/xy/community/dao/UserDao.java
+++ src/main/java/com/xy/community/dao/UserDao.java
@@ -1,9 +1,12 @@
 package com.xy.community.dao;
 
 import com.xy.community.model.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao {
     void insert(User user);
+
+    User findByToken(@Param("token") String token);
 }
